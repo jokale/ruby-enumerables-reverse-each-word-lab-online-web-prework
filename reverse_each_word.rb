dab1f97264b4ffee95b06aa5219aee07701210c6
Joanna E K <kalemajoanna@gmail.com>
2019-11-10 17:08:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,3 @@ new_array = []
   end
   reverse_string = new_array.join(' ')
 end 
-# def reverse_each_word(str)
-#   new_array = []
-#   str_array = str.split(' ')
-#   str_array.collect do |word|
-#     new_array << word.reverse
-#   end
-#   reverse_string = new_array.join(' ')
-# end 
- 
